7ed6ac7f650e149330dab6dadf72e9e23515e7ae
unknown <baurzhanov.b.08@gmail.com>
2020-04-10 03:53:14 +0600

    proj

---
+++ OnlineShop/src/app/cart.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private itemsUrl = 'api/iteams';
+  items = [];
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  deleteHero (item: CartComponent | number): Observable<CartComponent> {
+    const id = typeof item === 'number' ? item : item;
+    const url = `${this.itemsUrl}/${id}`;
+
+    return this.http.delete<CartComponent>(url, this.httpOptions).pipe(
+      catchError(this.handleError<CartComponent>('delete'))
+    );
+  }
+  handleError<T>(arg0: string): (err: any, caught: Observable<any>) => import("rxjs").ObservableInput<any> {
+    throw new Error("Method not implemented.");
+  }
+
+
+
+  constructor(
+    private http: HttpClient
+  ) { }
+}
---
+++ OnlineShop/src/app/cart/cart.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {CartService} from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items: CartComponent[];
+
+
+  constructor(
+    private cartService: CartService,
+  ) { }
+
+  ngOnInit(): void {
+    this.items = this.cartService.getItems();
+  }
+  delete(item: CartComponent) :void{
+    this.items = this.items.filter( (item: any) => item !==this.items);
+    this.cartService.deleteHero(item).subscribe();
+  }
+}
